07e778807025885519ef8b9c2136df570fa8d21f
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-13 11:52:45 -0700

    adds aknowlegable server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import http from "node:http";
 import app from "./lib/app.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 8080;
+const hostname = process.env.APP_HOST || 'localhost';
 
-server.listen(PORT, () => {
-  console.log(server.address());
+server.listen(port, () => {
+  console.log(`👂 Server is listening on ${JSON.stringify(server.address())}`);
 });
