1c0e271535278966286f3cda2a142cafb8c250ee
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-13 13:00:34 -0700

    getting first route for GET api/v1/cats

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import http from "node:http";
-import app from "./lib/app.js";
+import http from 'node:http';
+import app from './lib/app.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,6 +9,6 @@ const server = http.createServer(app);
 const port = process.env.APP_PORT || 8080;
 const hostname = process.env.APP_HOST || 'localhost';
 
-server.listen(port, () => {
+server.listen(port, hostname, () => {
   console.log(`👂 Server is listening on ${JSON.stringify(server.address())}`);
 });
